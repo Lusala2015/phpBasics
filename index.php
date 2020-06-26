3b33fc9de6222d7615b5b45579cc350a965ca6f1
Lusala2015 <lusalamark17@gmail.com>
2020-06-26 07:00:07 +0300

    styling the web page

--- index.php
+++ index.php
@@ -165,14 +165,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mark Coding </title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <header>
     <h1>Mark Coding</h1>
     <ul>
         <li><a href="index.php">Page 1</a></li>
-        <a href="index.php">Page 2</a>
+        <li><a href="index.php">Page 2</a></li>
     </ul>
 </header>
 <h4 class="page1">Page 1</h4>
