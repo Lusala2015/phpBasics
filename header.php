b9811b401a57b27355974f615af3710aaf0f56cc
Lusala2015 <lusalamark17@gmail.com>
2020-06-26 07:01:19 +0300

    createing a header.php file

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mark Coding </title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<header>
+    <h1>Mark Coding</h1>
+    <ul>
+        <li><a href="index.php">Page 1</a></li>
+        <li><a href="index.php">Page 2</a></li>
+    </ul>
+</header>
--- index.php
+++ index.php
@@ -159,22 +159,7 @@
     <input type="submit" value = "Log in">
 </form> -->
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mark Coding </title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-<header>
-    <h1>Mark Coding</h1>
-    <ul>
-        <li><a href="index.php">Page 1</a></li>
-        <li><a href="index.php">Page 2</a></li>
-    </ul>
-</header>
+
 <h4 class="page1">Page 1</h4>
 <footer>
 <p>This clas is created with love </p>
